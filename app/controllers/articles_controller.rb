68d6e381a4420a8e671e42e7fc430725d171d63f
Velichko Stoev <veli4kostoev@gmail.com>
2015-09-25 19:57:18 +0300

    installed simple_form and article home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
+
   def index
+  end
+
+  def show
   end
 
   def new
@@ -17,7 +22,11 @@ class ArticlesController < ApplicationController
 
   private
 
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
-    params.require(:acticle).permit(:title, :content)
+    params.require(:article).permit(:title, :content)
   end
 end
